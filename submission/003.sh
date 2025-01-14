57b0424106952fc1b259cdf0255d8804e10b85f3
Gustavo Lima <guts.santana@gmail.com>
2025-01-14 20:13:51 -0300

    teste 003

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,5 @@
 # How many new outputs were created by block 123,456?
+block_hash=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_231 -rpcpassword=Fc3IHA1VGEyI getblockhash 123456)
+block_info=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_231 -rpcpassword=Fc3IHA1VGEyI getblock "$block_hash" 2)
+total_outputs=$(echo "$block_info" | jq '[.tx[] | .vout | length] | add')
+echo "$total_outputs"
